4105b8ae29f5fb20fabc02403cc30685a776ec8f
kambire <kambire@users.noreply.github.com>
2026-03-30 23:11:54 -0300

    Fix: Add missing Spanish translations for heaven theme

---
+++ application/themes/heaven/language/spanish/theme.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+# Lang array
+$lang = [
+    /**
+     * - RTL -----------------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'isRTL' => 0,
+
+    /**
+     * - General -------------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'general_or'         => 'o',
+    'general_icon'       => 'Icono',
+    'general_okay'       => 'Bien',
+    'general_accept'     => 'Aceptar',
+    'general_reject'     => 'Rechazar',
+    'general_cancel'     => 'Cancelar',
+    'general_online'     => 'Conectado',
+    'general_offline'    => 'Desconectado',
+    'general_loading'    => 'Cargando...',
+    'general_readMore'   => 'Leer más',
+    'general_userAvatar' => 'Avatar de %s',
+
+    /**
+     * - Main Template -------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'btn_logout'           => 'Cerrar sesión',
+    'btn_register'         => 'Registrarse',
+    'btn_downloadLauncher' => 'Descargar launcher',
+
+    'wall_playNow'         => 'Jugar ahora',
+    'wall_userOnline'      => 'Explorar %spanel de cuenta%s o',
+    'wall_userOffline'     => '¿Ya eres miembro? %siniciar sesión%s o',
+    'wall_playersOnline'   => 'Jugadores conectados',
+
+    'footer_copyright'     => '%s&copy;%s Derechos de autor %s',
+
+    /**
+     * - Module: News --------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'news_pageTitle' => 'Últimas noticias',
+
+    /**
+     * - Module: Sidebox toppvp ----------------------------------
+     * -----------------------------------------------------------
+     */
+    'toppvp_rank'         => 'NR',
+    'toppvp_player'       => 'Jugador',
+    'toppvp_player_kills' => 'Muertes'
+];
